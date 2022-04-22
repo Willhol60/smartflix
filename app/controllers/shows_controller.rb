61f53177321620987fb048b0e90c686f2d21cd0e
Willhol60 <willholmes60@gmail.com>
2022-04-22 09:27:24 +0100

    render shows (with limit on #) to index page

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,2 +1,16 @@
+require "csv"
+
 class ShowsController < ApplicationController
+    def index
+        @shows = []
+        CSV.foreach("lib/assets/netflix_titles.csv", headers: true, header_converters: :symbol) do |row|
+            @shows << {
+                title: row[:title],
+                year: row[:release_year],
+                type: row[:type],
+                description: row[:description]
+            }
+            break if @shows.length == 10
+        end
+    end
 end
--- test/system/shows_test.rb
+++ test/system/shows_test.rb
@@ -6,4 +6,11 @@ class ShowsTest < ApplicationSystemTestCase
   
     assert_selector "h1", text: "Smartflix"
   end
+
+  test "the index page displays every show" do
+    visit root_path
+  
+    assert_text "Dick Johnson Is Dead"
+    assert_text "Blood & Water"
+  end
 end
