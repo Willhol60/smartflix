48f0b9433ec668a2ae67c1b02e1968c12af4ad5c
Willhol60 <willholmes60@gmail.com>
2022-04-27 12:16:42 +0100

    removing comments and adding files to gitignore

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,15 +4,5 @@ class PagesController < ApplicationController
     include PagesHelper 
     def index
         @shows = csv_data
-        # @shows = []
-        # CSV.foreach("lib/assets/netflix_titles.csv", headers: true, header_converters: :symbol) do |row|
-        #     @shows << {
-        #         title: row[:title],
-        #         year: row[:release_year],
-        #         type: row[:type],
-        #         description: row[:description]
-        #     }
-        #     break if @shows.length == 10
-        # end
     end
 end
