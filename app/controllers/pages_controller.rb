92e28f79357d0f9e95eab1733143c624dae3bac2
Willhol60 <willholmes60@gmail.com>
2022-04-27 12:09:29 +0100

    using the sassc gem instead of webpacker and extracting the pages controller logic to a pageshelper

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,18 @@
 require "csv"
 
 class PagesController < ApplicationController
+    include PagesHelper 
     def index
-        @shows = []
-        CSV.foreach("lib/assets/netflix_titles.csv", headers: true, header_converters: :symbol) do |row|
-            @shows << {
-                title: row[:title],
-                year: row[:release_year],
-                type: row[:type],
-                description: row[:description]
-            }
-            break if @shows.length == 10
-        end
+        @shows = csv_data
+        # @shows = []
+        # CSV.foreach("lib/assets/netflix_titles.csv", headers: true, header_converters: :symbol) do |row|
+        #     @shows << {
+        #         title: row[:title],
+        #         year: row[:release_year],
+        #         type: row[:type],
+        #         description: row[:description]
+        #     }
+        #     break if @shows.length == 10
+        # end
     end
 end
