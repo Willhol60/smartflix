f3c95c5d546f128333ce9662bb058f69c357ae63
Willhol60 <willholmes60@gmail.com>
2022-04-27 12:32:30 +0100

    moving tests into single spec

--- test/system/shows_test.rb
+++ test/system/shows_test.rb
@@ -1,15 +1,10 @@
 require "application_system_test_case"
 
 class ShowsTest < ApplicationSystemTestCase
-  test "visiting the index" do
-    visit root_path
-  
-    assert_selector "h1", text: "Smartflix"
-  end
-
   test "the index page displays every show" do
     visit root_path
   
+    assert_selector "h1", text: "Smartflix"
     assert_text "Dick Johnson Is Dead"
     assert_text "Blood & Water"
   end
