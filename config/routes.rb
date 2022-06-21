1614253d275447916cd700ba48cdcebecd46d2d6
Willhol60 <willholmes60@gmail.com>
2022-06-21 16:30:44 +0200

    oauth setup (facebook)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
